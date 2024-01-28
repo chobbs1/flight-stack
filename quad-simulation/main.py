6fd2e6bc867c95ba0f363bcb1c7b65d329665089
Chris Hobbs <c.hobbs@skybornetech.com>
2024-01-28 18:46:37 +0100

    added axes and rotations

--- quad-simulation/main.py
+++ quad-simulation/main.py
@@ -1,13 +1,17 @@
-from quad_model import QuadCopter
+from quad_model import QuadCopter, InertialFrame
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 def main():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+
+    inertial = InertialFrame()
     quad = QuadCopter(0, 0, 0)
 
     quad.plot_quad(ax)
+    inertial.plot_axes(ax)
+
     plt.show()
 
 if __name__ == "__main__":
--- quad-simulation/quad_model.py
+++ quad-simulation/quad_model.py
@@ -12,44 +12,43 @@ class QuadCopter:
     def __init__(self, x, y, z):
         self.r_O_O1_f_inertial = np.array([x, y, z])
 
-        # self.O_body = np.array([0, 0, 0])
-        # self.A_body = np.array([self.arm_length, 0, 0])
-        # self.B_body = np.array([0, self.arm_length, 0])
-        # self.C_body = np.array([0, -self.arm_length, 0])
-        # self.D_body = np.array([-self.arm_length, 0, 0])
+        self.O1_body = np.array([0, 0, 0])
+        self.A_body = np.array([self.arm_length, 0, 0])
+        self.B_body = np.array([0, self.arm_length, 0])
+        self.C_body = np.array([0, -self.arm_length, 0])
+        self.D_body = np.array([-self.arm_length, 0, 0])
 
-        self.O_inert = np.array([0, 0, 0])
-        self.A_inert = np.array([self.arm_length, 0, 0])
-        self.B_inert = np.array([0, self.arm_length, 0])
-        self.C_inert = np.array([0, -self.arm_length, 0])
-        self.D_inert = np.array([-self.arm_length, 0, 0])
-
-
-    # euler_angles = np.array([0, 0, 0])
-    # rotation = Rotation.from_euler('ZYX', euler_angles, degrees=True)
-
+        self.euler_angles = np.array([0, 0, 30])
+        
     def plot_quad(self, ax):
+        R_body2inertial = Rotation.from_euler('ZYX', self.euler_angles, degrees=True).as_matrix()
 
-        r_OA = np.array([[self.O_inert[0], self.A_inert[0]], 
-                    [self.O_inert[1], self.A_inert[1]], 
-                    [self.O_inert[2], self.A_inert[2]]])
+        self.O1_inert = np.dot(R_body2inertial, self.O1_body)
+        self.A_inert = np.dot(R_body2inertial, self.A_body)
+        self.B_inert = np.dot(R_body2inertial, self.B_body)
+        self.C_inert = np.dot(R_body2inertial, self.C_body)
+        self.D_inert = np.dot(R_body2inertial, self.D_body)
 
-        r_OB = np.array([[self.O_inert[0], self.B_inert[0]], 
-                    [self.O_inert[1], self.B_inert[1]], 
-                    [self.O_inert[2], self.B_inert[2]]])
+        r_O1_A_inert = np.array([[self.O1_inert[0], self.A_inert[0]], 
+                    [self.O1_inert[1], self.A_inert[1]], 
+                    [self.O1_inert[2], self.A_inert[2]]])
 
-        r_OC = np.array([[self.O_inert[0], self.C_inert[0]], 
-                    [self.O_inert[1], self.C_inert[1]], 
-                    [self.O_inert[2], self.C_inert[2]]])
+        r_O1_B_inert = np.array([[self.O1_inert[0], self.B_inert[0]], 
+                    [self.O1_inert[1], self.B_inert[1]], 
+                    [self.O1_inert[2], self.B_inert[2]]])
 
-        r_OD = np.array([[self.O_inert[0], self.D_inert[0]], 
-                    [self.O_inert[1], self.D_inert[1]], 
-                    [self.O_inert[2], self.D_inert[2]]])
+        r_O1_C_inert = np.array([[self.O1_inert[0], self.C_inert[0]], 
+                    [self.O1_inert[1], self.C_inert[1]], 
+                    [self.O1_inert[2], self.C_inert[2]]])
 
-        ax.plot(r_OA[0], r_OA[1], r_OA[2], color=self.color, linewidth=self.thick)
-        ax.plot(r_OB[0], r_OB[1], r_OB[2], color=self.color, linewidth=self.thick)
-        ax.plot(r_OC[0], r_OC[1], r_OC[2], color=self.color, linewidth=self.thick)
-        ax.plot(r_OD[0], r_OD[1], r_OD[2], color=self.color, linewidth=self.thick)
+        r_O1_D_inert = np.array([[self.O1_inert[0], self.D_inert[0]], 
+                    [self.O1_inert[1], self.D_inert[1]], 
+                    [self.O1_inert[2], self.D_inert[2]]])
+
+        ax.plot(r_O1_A_inert[0], r_O1_A_inert[1], r_O1_A_inert[2], color=self.color, linewidth=self.thick)
+        ax.plot(r_O1_B_inert[0], r_O1_B_inert[1], r_O1_B_inert[2], color=self.color, linewidth=self.thick)
+        ax.plot(r_O1_C_inert[0], r_O1_C_inert[1], r_O1_C_inert[2], color=self.color, linewidth=self.thick)
+        ax.plot(r_O1_D_inert[0], r_O1_D_inert[1], r_O1_D_inert[2], color=self.color, linewidth=self.thick)
 
         ax.set_xlabel('X Axis')
         ax.set_ylabel('Y Axis')
@@ -60,7 +59,16 @@ class QuadCopter:
         ax.set_ylim(-axes_length, axes_length)
         ax.set_zlim(0, 2 * axes_length)
 
+class InertialFrame:
 
+    def __init__(self):
+        self.axes_length = 2
+        self.color = "black" 
+        self.thick = 1
 
+    def plot_axes(self, ax):
+        ax.quiver(0, 0, 0, self.axes_length, 0, 0, color=self.color)
+        ax.quiver(0, 0, 0, 0, self.axes_length, 0, color=self.color)
+        ax.quiver(0, 0, 0, 0, 0, -self.axes_length, color=self.color)
     
 
